51501c6477e1722cb0f98c236a85f762e1b1bde1
John Biddiscombe <biddisco@cscs.ch>
2025-10-29 16:15:09 +0100

    halocells: Adding (non functioning) pre-post of receives

--- src/Field/HaloCells.hpp
+++ src/Field/HaloCells.hpp
@@ -21,7 +21,8 @@ namespace ippl {
         }
 
         template <typename T, unsigned Dim, class... ViewArgs>
-        void HaloCells<T, Dim, ViewArgs...>::accumulateHalo_noghost(view_type& view, Layout_t* layout, int nghost) {
+        void HaloCells<T, Dim, ViewArgs...>::accumulateHalo_noghost(view_type& view,
+                                                                    Layout_t* layout, int nghost) {
             exchangeBoundaries<lhs_plus_assign>(view, layout, HALO_TO_INTERNAL_NOGHOST, nghost);
         }
         template <typename T, unsigned Dim, class... ViewArgs>
@@ -54,22 +55,70 @@ namespace ippl {
             // needed for the NOGHOST approach - we want to remove the ghost
             // cells on the boundaries of the global domain from the halo
             // exchange when we set HALO_TO_INTERNAL_NOGHOST
-            const auto domain = layout->getDomain();
+            const auto domain    = layout->getDomain();
             const auto& ldomains = layout->getHostLocalDomains();
 
-            size_t totalRequests = 0;
+            size_t sendRequests = 0;
             for (const auto& componentNeighbors : neighbors) {
-                totalRequests += componentNeighbors.size();
+                sendRequests += componentNeighbors.size();
             }
 
-            int me=Comm->rank();
+            int me = Comm->rank();
 
             using memory_space = typename view_type::memory_space;
             using buffer_type  = mpi::Communicator::buffer_type<memory_space>;
-            std::vector<MPI_Request> requests(totalRequests);
-            // sending loop
+            std::vector<MPI_Request> send_requests(sendRequests);
+            std::vector<MPI_Request> recv_requests;
+
             constexpr size_t cubeCount = detail::countHypercubes(Dim) - 1;
-            size_t requestIndex        = 0;
+
+            // ------------------------------------
+            // pre-post receives loop
+            // ------------------------------------
+            struct bc_irecv_data {
+                buffer_type pre_posted_recv_buf;
+                bound_type range;
+            };
+            std::vector<bc_irecv_data> pre_posted_bufs;
+            //
+            for (size_t index = 0; index < cubeCount; index++) {
+                int tag                        = mpi::tag::HALO + Layout_t::getMatchingIndex(index);
+                const auto& componentNeighbors = neighbors[index];
+                for (size_t i = 0; i < componentNeighbors.size(); i++) {
+                    int sourceRank = componentNeighbors[i];
+
+                    bound_type range;
+                    if (order == INTERNAL_TO_HALO) {
+                        range = recvRanges[index][i];
+                    } else if (order == HALO_TO_INTERNAL_NOGHOST) {
+                        range = sendRanges[index][i];
+
+                        for (size_t j = 0; j < Dim; ++j) {
+                            bool isLower = ((range.lo[j] + ldomains[me][j].first() - nghost)
+                                            == domain[j].min());
+                            bool isUpper = ((range.hi[j] - 1 + ldomains[me][j].first() - nghost)
+                                            == domain[j].max());
+                            range.lo[j] += isLower * (nghost);
+                            range.hi[j] -= isUpper * (nghost);
+                        }
+                    } else {
+                        range = sendRanges[index][i];
+                    }
+
+                    size_type nrecvs = range.size();
+
+                    // std::cout << haloData_m.get_buffer();
+                    buffer_type buf = comm.template getBuffer<memory_space, T>(nrecvs);
+                    MPI_Request request;
+                    // comm.recv(sourceRank, tag, haloData_m, *buf, nrecvs * sizeof(T), nrecvs);
+                    comm.irecv(sourceRank, tag, *buf, request, nrecvs * sizeof(T));
+                    pre_posted_bufs.push_back({buf, range});
+                    recv_requests.push_back(request);
+                }
+            }
+
+            // sending loop
+            size_t requestIndex = 0;
             for (size_t index = 0; index < cubeCount; index++) {
                 int tag                        = mpi::tag::HALO + index;
                 const auto& componentNeighbors = neighbors[index];
@@ -88,10 +137,9 @@ namespace ippl {
                         range = recvRanges[index][i];
 
                         for (size_t j = 0; j < Dim; ++j) {
-                            bool isLower = ((range.lo[j] + ldomains[me][j].first()
-                                            - nghost) == domain[j].min());
-                            bool isUpper = ((range.hi[j] - 1 + 
-                                            ldomains[me][j].first() - nghost)
+                            bool isLower = ((range.lo[j] + ldomains[me][j].first() - nghost)
+                                            == domain[j].min());
+                            bool isUpper = ((range.hi[j] - 1 + ldomains[me][j].first() - nghost)
                                             == domain[j].max());
                             range.lo[j] += isLower * (nghost);
                             range.hi[j] -= isUpper * (nghost);
@@ -105,53 +153,34 @@ namespace ippl {
 
                     buffer_type buf = comm.template getBuffer<memory_space, T>(nsends);
 
-                    comm.isend(targetRank, tag, haloData_m, *buf, requests[requestIndex++], nsends);
+                    comm.isend(targetRank, tag, haloData_m, *buf, send_requests[requestIndex++],
+                               nsends);
                     buf->resetWritePos();
                 }
             }
 
-            // receiving loop
-            for (size_t index = 0; index < cubeCount; index++) {
-                int tag                        = mpi::tag::HALO + Layout_t::getMatchingIndex(index);
-                const auto& componentNeighbors = neighbors[index];
-                for (size_t i = 0; i < componentNeighbors.size(); i++) {
-                    int sourceRank = componentNeighbors[i];
+            if (sendRequests > 0) {
+                // std::cout << "WaitAll for " << sendRequests << " " << send_requests.size()
+                //           << " buffers" << std::endl;
+                MPI_Waitall(sendRequests, send_requests.data(), MPI_STATUSES_IGNORE);
+            }
 
-                    bound_type range;
-                    if (order == INTERNAL_TO_HALO) {
-                        range = recvRanges[index][i];
-                    } else if (order == HALO_TO_INTERNAL_NOGHOST) {
-                        range = sendRanges[index][i];
-
-                        for (size_t j = 0; j < Dim; ++j) {
-                            bool isLower = ((range.lo[j] + ldomains[me][j].first()
-                                            - nghost) == domain[j].min());
-                            bool isUpper = ((range.hi[j] - 1 + 
-                                            ldomains[me][j].first() - nghost)
-                                            == domain[j].max());
-                            range.lo[j] += isLower * (nghost);
-                            range.hi[j] -= isUpper * (nghost);
-                        }
-                    } else {
-                        range = sendRanges[index][i];
-                    }
-
-                    size_type nrecvs = range.size();
-
-                    buffer_type buf = comm.template getBuffer<memory_space, T>(nrecvs);
-
-                    comm.recv(sourceRank, tag, haloData_m, *buf, nrecvs * sizeof(T), nrecvs);
-                    buf->resetReadPos();
-
-                    unpack<Op>(range, view, haloData_m);
+            // ------------------------------------
+            // deserialize and unpack pre-posted receives
+            // ------------------------------------
+            if (pre_posted_bufs.size() > 0) {
+                // std::cout << "WaitAll for " << pre_posted_bufs.size() << " " <<
+                // recv_requests.size()
+                //           << " buffers" << std::endl;
+                MPI_Waitall(recv_requests.size(), recv_requests.data(), MPI_STATUSES_IGNORE);
+                for (auto& bc_data : pre_posted_bufs) {
+                    haloData_m.deserialize(*bc_data.pre_posted_recv_buf, bc_data.range.size());
+                    unpack<Op>(bc_data.range, view, haloData_m);
+                    comm.template freeBuffer(bc_data.pre_posted_recv_buf);
                 }
             }
 
-            if (totalRequests > 0) {
-                MPI_Waitall(totalRequests, requests.data(), MPI_STATUSES_IGNORE);
-            }
-            
-            comm.freeAllBuffers();
+            // comm.freeAllBuffers();
         }
 
         template <typename T, unsigned Dim, class... ViewArgs>
